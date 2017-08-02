2b8525f7a5ea33ca7bd058d4592a7eda1297339f
Jason Rogena <jasonrogena@gmail.com>
2017-08-02 11:43:29 +0300

    MT:JR| Deleting Old ETL Reports
    Add code for deleting old ETL reports from the server, and S3
    
    Fixes issue #6
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>
    Signed-off-by: Manu Tarus <etarus@ona.io>

--- facility_activity_report_cron.sh
+++ facility_activity_report_cron.sh
@@ -5,6 +5,7 @@ generateEtl() {
   # $3 - MySQL password
   # $4 - Slack channel to post message
   # $5 - Slack token to use
+  # $6 - Report life in days
   cd $1
 
   now="$(date +'%d_%m_%Y_%H_%M_%S')"
@@ -14,10 +15,11 @@ generateEtl() {
   destinationFile="etl/$file_name"
 
   mkdir -p $report_folder
+  find $report_folder -mtime +$6 -exec rm {} \;
   /usr/bin/mysql -u $2 -p$3 < ./facility_activity_report_dml.sql
   /usr/bin/mysql -u $2 -p$3 -e 'select * from path_zambia_etl.facility_activity_report' | sed  's/\t/,/g' > $sourceFile
-  /usr/bin/aws s3 cp $sourceFile s3://opensrp/reports/$destinationFile --acl public-read
+  /usr/bin/aws s3 sync $report_folder s3://opensrp/reports/etl --acl public-read --delete
   /usr/bin/curl -F username="PATH Zambia ETL" -F text="https://s3.amazonaws.com/opensrp/reports/$destinationFile" -F channel=$4 -F token="$5" https://slack.com/api/chat.postMessage
 }
 
-generateEtl $1 $2 $3 $4 $5
+generateEtl $1 $2 $3 $4 $5 $6
