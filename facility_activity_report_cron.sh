4f55df6a25067c2c5ba26b1681256baced8959e4
Jason Rogena <jasonrogena@gmail.com>
2017-08-14 10:55:59 +0300

    Not Sending Slack Notifications
    Check if the slack channel variable is set in
    facility_activity_report_cron.sh before trying to send out the Slack
    notification
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- facility_activity_report_cron.sh
+++ facility_activity_report_cron.sh
@@ -3,9 +3,9 @@ generateEtl() {
   local pathToScripts=$1
   local mysqlUser=$2
   local mysqlPassword=$3
-  local slackChannel=$4
-  local slackToken=$5
-  local reportLife=$6
+  local reportLife=$4
+  local slackChannel=$5
+  local slackToken=$6
 
   cd $pathToScripts
 
@@ -20,7 +20,9 @@ generateEtl() {
   /usr/bin/mysql -u $mysqlUser -p$mysqlPassword < ./facility_activity_report_dml.sql
   /usr/bin/mysql -u $mysqlUser -p$mysqlPassword -e 'select * from path_zambia_etl.facility_activity_report' | sed  's/\t/,/g' > $sourceFile
   /usr/bin/aws s3 sync $report_folder s3://opensrp/reports/etl --acl public-read --delete
-  /usr/bin/curl -F username="PATH Zambia ETL" -F text="https://s3.amazonaws.com/opensrp/reports/$destinationFile" -F channel=$slackChannel -F token="$slackToken" https://slack.com/api/chat.postMessage
+  if [ -z ${slackChannel+x} ]; then echo "Not sending URL to Slack"; else
+    /usr/bin/curl -F username="PATH Zambia ETL" -F text="https://s3.amazonaws.com/opensrp/reports/$destinationFile" -F channel=$slackChannel -F token="$slackToken" https://slack.com/api/chat.postMessage;
+  fi
 }
 
 generateEtl $1 $2 $3 $4 $5 $6
