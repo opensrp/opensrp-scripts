0f7429f052469be2c9f83e9be44b18b0eba18006
Jason Rogena <jasonrogena@gmail.com>
2017-08-14 10:48:24 +0300

    Defensive Bash Script
    Modify the cron job script for generating the ETL CSVs to make it more
    defensive. See this[1] post on defensive bash programming
    
    1 - http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- facility_activity_report_cron.sh
+++ facility_activity_report_cron.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 generateEtl() {
-  # $1 - Path to ETL scripts
-  # $2 - MySQL user
-  # $3 - MySQL password
-  # $4 - Slack channel to post message
-  # $5 - Slack token to use
-  # $6 - Report life in days
-  cd $1
+  local pathToScripts=$1
+  local mysqlUser=$2
+  local mysqlPassword=$3
+  local slackChannel=$4
+  local slackToken=$5
+  local reportLife=$6
+
+  cd $pathToScripts
 
   now="$(date +'%d_%m_%Y')"
   file_name="facility_activity_report_$now".csv
@@ -15,11 +16,11 @@ generateEtl() {
   destinationFile="etl/$file_name"
 
   mkdir -p $report_folder
-  find $report_folder -mtime +$6 -exec rm {} \;
-  /usr/bin/mysql -u $2 -p$3 < ./facility_activity_report_dml.sql
-  /usr/bin/mysql -u $2 -p$3 -e 'select * from path_zambia_etl.facility_activity_report' | sed  's/\t/,/g' > $sourceFile
+  find $report_folder -mtime +$reportLife -exec rm {} \;
+  /usr/bin/mysql -u $mysqlUser -p$mysqlPassword < ./facility_activity_report_dml.sql
+  /usr/bin/mysql -u $mysqlUser -p$mysqlPassword -e 'select * from path_zambia_etl.facility_activity_report' | sed  's/\t/,/g' > $sourceFile
   /usr/bin/aws s3 sync $report_folder s3://opensrp/reports/etl --acl public-read --delete
-  /usr/bin/curl -F username="PATH Zambia ETL" -F text="https://s3.amazonaws.com/opensrp/reports/$destinationFile" -F channel=$4 -F token="$5" https://slack.com/api/chat.postMessage
+  /usr/bin/curl -F username="PATH Zambia ETL" -F text="https://s3.amazonaws.com/opensrp/reports/$destinationFile" -F channel=$slackChannel -F token="$slackToken" https://slack.com/api/chat.postMessage
 }
 
 generateEtl $1 $2 $3 $4 $5 $6
