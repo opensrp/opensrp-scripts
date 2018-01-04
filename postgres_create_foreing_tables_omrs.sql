c8584314b7b0c43032a3dc978504343ec681fc17
manutarus <manutarus@gmail.com>
2018-01-04 19:04:56 +0300

    added foreign tables and views for omrs data

---
+++ postgres_create_foreing_tables_omrs.sql
@@ -0,0 +1,120 @@
+﻿
+#creating FOREIGN TABLES to get data from openmrs for concepts, users and location this tables
+CREATE FOREIGN TABLE location (
+location_id integer,
+name varchar(255)  DEFAULT '',
+description varchar(255),
+address1 varchar(255),
+address2 varchar(255),
+city_village varchar(255),
+state_province varchar(255),
+postal_code varchar(50),
+country varchar(50),
+latitude varchar(50),
+longitude varchar(50),
+creator integer ,
+date_created varchar(50),
+county_district varchar(255),
+address3 varchar(255),
+address4 varchar(255),
+address5 varchar(255),
+address6 varchar(255),
+retired smallint ,
+retired_by integer,
+date_retired varchar(50),
+retire_reason varchar(255),
+parent_location integer,
+uuid char(38),
+changed_by integer,
+date_changed varchar(50))
+SERVER mysql_server
+OPTIONS (dbname 'openmrs', table_name 'location');
+
+#GRANT ALL PRIVILEGES ON TABLE ﻿location_tag_map TO ﻿﻿﻿﻿etlgenerator;
+CREATE FOREIGN TABLE location_tag_map (
+location_id integer,
+location_tag_id integer)
+SERVER mysql_server
+OPTIONS (dbname 'openmrs', table_name 'location_tag_map');
+
+
+CREATE FOREIGN TABLE users (
+user_id integer,
+system_id varchar(50),
+username varchar(50),
+password varchar(128),
+salt varchar(128),
+secret_question varchar(255),
+secret_answer varchar(255),
+creator integer,
+date_created varchar(50),
+changed_by integer,
+date_changed datetime,
+person_id integer,
+retired integer,
+retired_by integer,
+date_retired varchar(50),
+retire_reason varchar(255),
+uuid char(38)
+) SERVER mysql_server
+OPTIONS (dbname 'openmrs', table_name 'users');
+
+CREATE FOREIGN TABLE concept_name (
+concept_id integer,
+name varchar(255),
+locale varchar(50),
+creator integer,
+date_created varchar(50),
+concept_name_id integer,
+voided smallint,
+voided_by integer,
+date_voided varchar(50),
+void_reason varchar(255),
+uuid char(38),
+concept_name_type varchar(50),
+locale_preferred smallint
+)
+SERVER mysql_server
+OPTIONS (dbname 'openmrs', table_name 'concept_name');
+
+
+CREATE FOREIGN TABLE provider (
+provider_id integer,
+person_id integer,
+name varchar(255),
+identifier varchar(255),
+creator integer ,
+date_created datetime ,
+changed_by integer,
+date_changed datetime,
+retired smallint,
+retired_by integer,
+date_retired datetime,
+retire_reason varchar(255),
+uuid char(38))
+SERVER mysql_server
+OPTIONS (dbname 'openmrs', table_name 'provider');
+
+CREATE FOREIGN TABLE person_name (
+person_name_id integer,
+preferred smallint,
+person_id integer,
+prefix varchar(50),
+given_name varchar(50),
+middle_name varchar(50),
+family_name_prefix varchar(50),
+family_name varchar(50),
+family_name2 varchar(50),
+family_name_suffix varchar(50),
+degree varchar(50),
+creator integer,
+date_created varchar(50),
+voided varchar(50),
+voided_by integer,
+date_voided varchar(50),
+void_reason varchar(255),
+changed_by integer,
+date_changed varchar(50),
+uuid char(38))
+SERVER mysql_server
+OPTIONS (dbname 'openmrs', table_name 'person_name');
---
+++ postgres_create_views.sql
@@ -0,0 +1,102 @@
+﻿#creating views to hold data extracted from json docs from couchDB
+
+CREATE MATERIALIZED VIEW event AS
+SELECT
+  doc->>'baseEntityId' as base_entity_id,
+  doc->>'eventDate' as event_date,
+  doc->>'eventType' as event_type,
+  doc->>'providerId' as provider_id,
+  doc->>'locationId' as location_id,
+  doc->>'formSubmissionId' as form_submission_id,
+  doc->>'dateCreated' as date_created
+FROM public.couchdb where doc @> '{"type":"Event"}';
+
+CREATE INDEX i_event_base_entity_id  ON event (base_entity_id);
+
+
+CREATE MATERIALIZED VIEW vaccination AS
+SELECT
+  doc->>'baseEntityId' as base_entity_id,
+  doc->>'eventDate' as event_date,
+  doc->>'providerId' as provider_id,
+  doc->>'locationId' as location_id,
+  doc->>'dateCreated' as date_created,
+  doc->'obs'->0->>'formSubmissionField' as vaccine,
+  doc->'obs'->0->'values'->>0 as v_date,
+  doc->'obs'->1->>'formSubmissionField' as dose,
+  doc->'obs'->1->'values'->>0 as dose_value
+FROM public.couchdb where doc @> '{"type":"Event", "eventType":"Vaccination"}';
+
+CREATE INDEX i_vaccination_base_entity_id  ON vaccination (base_entity_id);
+
+CREATE MATERIALIZED VIEW weight AS
+SELECT
+  doc->>'baseEntityId' as base_entity_id,
+  doc->>'eventDate' as event_date,
+  doc->>'providerId' as provider_id,
+  doc->>'locationId' as location_id,
+  doc->>'dateCreated' as date_created,
+  doc->'obs'->0->'values'->>0 as weight,
+  doc->'obs'->1->'values'->>0 as zscore
+FROM public.couchdb where doc @> '{"type":"Event", "eventType":"Growth Monitoring"}';
+
+CREATE INDEX i_weight_base_entity_id  ON weight (base_entity_id);
+
+
+CREATE MATERIALIZED VIEW birth_event AS
+SELECT
+  doc->>'baseEntityId' as base_entity_id,
+  doc->>'eventDate' as event_date,
+  doc->>'providerId' as provider_id,
+  doc->>'locationId' as location_id,
+  doc->>'dateCreated' as date_created
+FROM public.couchdb where doc @> '{"type":"Event", "eventType":"Birth Registration"}';;
+
+
+CREATE MATERIALIZED VIEW birth_event_obs AS
+SELECT doc->> 'baseEntityId' as base_entity_id,
+       elements ->> 'formSubmissionField' as form_submission_field,
+       elements -> 'values'->>0 as value
+FROM public.couchdb
+  CROSS JOIN jsonb_array_elements(doc->'obs') elements WHERE doc @> '{"type":"Event", "eventType":"Birth Registration"}';
+
+CREATE INDEX i_birth_event_obs_base_entity_id  ON birth_event_obs (base_entity_id);
+CREATE INDEX i_birth_event_obs_form_submission_field  ON birth_event_obs (form_submission_field);
+
+CREATE MATERIALIZED VIEW client AS
+SELECT
+  doc->>'baseEntityId' as base_entity_id,
+  doc->>'dateCreated' as date_created,
+  doc->'identifiers'->>'ZEIR_ID' as zeir_id,
+  doc->>'firstName' as first_name,
+  doc->>'middleName' as middle_name,
+  doc->>'lastName' as last_name,
+  doc->>'gender' as gender,
+  doc->>'birthdate' as birth_date,
+  doc->'addresses'->0->'addressFields'->>'address3' as health_facility,
+  doc->'addresses'->0->'addressFields'->>'address2' as residential_address,
+  doc->'attributes'->>'Home_Facility' as home_facility,
+  doc->'relationships'->'mother'->>0 as mother
+FROM public.couchdb where doc @> '{"type":"Client"}';;
+
+CREATE UNIQUE INDEX ui_client_bed
+  ON client (base_entity_id);
+
+#refreshing the views to fetch updates from couchdb
+CREATE OR REPLACE FUNCTION refresh_all_materialized_views(schema_arg TEXT DEFAULT 'public')
+RETURNS INT AS $$
+DECLARE
+r RECORD;
+BEGIN
+RAISE NOTICE 'Refreshing materialized view in schema %', schema_arg;
+FOR r IN SELECT matviewname FROM pg_matviews WHERE schemaname = schema_arg
+LOOP
+RAISE NOTICE 'Refreshing %.%', schema_arg, r.matviewname;
+EXECUTE 'REFRESH MATERIALIZED VIEW ' || schema_arg || '.' || r.matviewname;
+END LOOP;
+
+RETURN 1;
+END
+$$ LANGUAGE plpgsql;
+
+
