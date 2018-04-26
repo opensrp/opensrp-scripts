669c37a3153fdc3fabc5418547b6fab95f57f9f9
Samuel Githengi <samuel.githengi@gmail.com>
2018-04-26 15:48:26 +0300

    Update main table before metadata because lookup uses metadata

--- hashing_data/update_client_references.sql
+++ hashing_data/update_client_references.sql
@@ -11,25 +11,24 @@ BEGIN
       RAISE NOTICE 'baseEntityId %',rec.original_id;
       --Update events
 
-      UPDATE core.event_metadata SET base_entity_id=rec.generated_id WHERE base_entity_id=rec.original_id;
-
       UPDATE core.event SET json=jsonb_set(json,'{baseEntityId}',concat('"',rec.generated_id,'"')::jsonb,FALSE )
       where id IN (SELECT event_id FROM core.event_metadata WHERE base_entity_id=rec.original_id);
 
-      --Update actions
+      UPDATE core.event_metadata SET base_entity_id=rec.generated_id WHERE base_entity_id=rec.original_id;
 
-      UPDATE core.action_metadata SET base_entity_id=rec.generated_id WHERE base_entity_id=rec.original_id;
+      --Update actions
 
       UPDATE core.action SET json=jsonb_set(json,'{baseEntityId}',concat('"',rec.generated_id,'"')::jsonb,FALSE )
       where id IN (SELECT action_id FROM core.action_metadata WHERE base_entity_id=rec.original_id);
 
-       --Update alerts
+      UPDATE core.action_metadata SET base_entity_id=rec.generated_id WHERE base_entity_id=rec.original_id;
 
-      UPDATE core.alert_metadata SET base_entity_id=rec.generated_id WHERE base_entity_id=rec.original_id;
+       --Update alerts
 
       UPDATE core.alert SET json=jsonb_set(json,'{entityId}',concat('"',rec.generated_id,'"')::jsonb,FALSE )
       where id IN (SELECT alert_id FROM core.alert_metadata WHERE base_entity_id=rec.original_id);
 
+      UPDATE core.alert_metadata SET base_entity_id=rec.generated_id WHERE base_entity_id=rec.original_id;
 
        --Update multimedia
 
