5b186826f9f068fd6790cfe8ff0c645a72909d79
Manu Tarus <manutarus@gmail.com>
2018-07-26 17:02:48 +0300

    mVacc ETL scripts

---
+++ mVacc/mvacc-etl/caregivers_query.sql
@@ -0,0 +1,7 @@
+﻿SELECT m_zeir_id          AS "m_zeir_id",
+       mother_first_name  AS "Mother’s First Name",
+       mother_second_name AS "Mother’s Last Name",
+       phone_number       AS "Phone number",
+       rapidpro_uuid      AS "RapidProUuid"
+       
+FROM mvacc_mother;
---
+++ mVacc/mvacc-etl/mvacc_tables.sql
@@ -0,0 +1,36 @@
+﻿-- creating TABLES for mVacc variables and ETL process
+
+DROP TABLE IF EXISTS mvacc_mother;
+CREATE TABLE mvacc_mother (
+  mother_id          SERIAL,
+  m_zeir_id          VARCHAR(50) UNIQUE,
+  mother_first_name  VARCHAR(100),
+  mother_second_name VARCHAR(100),
+  phone_number       VARCHAR(15),
+  rapidpro_uuid      VARCHAR(100),
+  PRIMARY KEY (mother_id)
+);
+
+CREATE INDEX i_mother_id ON mvacc_mother (mother_id);
+CREATE INDEX i_rapidpro_uuid ON mvacc_mother (rapidpro_uuid);
+
+DROP TABLE IF EXISTS mvacc_child_mother_map;
+CREATE TABLE mvacc_child_mother_map (
+  child_mother_map_id  SERIAL,
+  mother_id            INTEGER,
+  zeir_id              VARCHAR(15) UNIQUE,
+  PRIMARY KEY (child_mother_map_id)
+);
+CREATE INDEX i_child_mother_map_id ON mvacc_child_mother_map (child_mother_map_id);
+CREATE INDEX i_mother ON mvacc_child_mother_map (mother_id);
+
+
+DROP TABLE IF EXISTS mvacc_variables;
+CREATE TABLE mvacc_variables (
+  varial_id  SERIAL,
+  key            VARCHAR(50) UNIQUE,
+  value          VARCHAR(500),
+  PRIMARY KEY (varial_id)
+);
+CREATE INDEX i_varial_id ON mvacc_variables (varial_id);
+CREATE INDEX i_key ON mvacc_variables (key);
