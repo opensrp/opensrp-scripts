ca8a3cc9e2f4b0e07fc409dda9b5308e5fab7f8c
Manu Tarus <manutarus@gmail.com>
2019-01-25 16:38:14 +0300

    generating events

--- mVacc/groovy/VaccinesToArray.groovy
+++ mVacc/groovy/VaccinesToArray.groovy
@@ -18,20 +18,34 @@
 //session.transfer(flowFile,REL_SUCCESS)
 
 import groovy.json.JsonSlurper
+import org.codehaus.groovy.classgen.genArrays
 
+import java.text.SimpleDateFormat
+
+def conceptMap = [
+        'BCG'    : '886AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        'OPV'    : '783AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        'PCV'    : '162342AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        'Penta'  : '1685AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        'Rota'   : '159698AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        'Measles': '36AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        'MR'     : '162586AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+]
 
 def jsonSlurper = new JsonSlurper()
-def test = '{"clientUUID" : "be854618-aa5c-472d-9c27-797de3709de0","opensrp_vaccines" : "[{},{},{\\"vaccine_name\\":\\"OPV\\",\\"date_administered\\":\\"2018-05-20\\",\\"sequence\\":\\"0\\"},{\\"vaccine_name\\":\\"BCG\\",\\"date_administered\\":\\"2018-05-20\\",\\"sequence\\":\\"1\\"}]","mvac_all_vaccines" : "421/2017 + BCG + OPV + DTP + PCV + Rota"}'
+def test = '{"providerId" : "biddemo","locationId" : "le854618-aa5c-472d-9c27-797de3709de0","clientUUID" : "be854618-aa5c-472d-9c27-797de3709de0","opensrp_vaccines" : "[{},{},{\\"vaccine_name\\":\\"OPV\\",\\"date_administered\\":\\"2018-05-20\\",\\"sequence\\":\\"0\\"},{\\"vaccine_name\\":\\"BCG\\",\\"date_administered\\":\\"2018-05-20\\",\\"sequence\\":\\"1\\"}]","mvac_all_vaccines" : "421/2017 + BCG + OPV + DTP + PCV + Rota"}'
 
 def inputJson = jsonSlurper.parseText(test)
 def opensrpVaccines = inputJson.opensrp_vaccines
+def clientUUID = inputJson.clientUUID
+def locationId = inputJson.locationId
+def providerId = inputJson.providerId
 def incomingFlowContent = inputJson.mvac_all_vaccines.replaceAll("\\s", "").replaceAll("\\+", ",")
 incomingFlowContent = incomingFlowContent.substring(incomingFlowContent.indexOf(',') + 1)
 def vaccineArray = incomingFlowContent.split(",")
 def opensrpVaccineArray = jsonSlurper.parseText(opensrpVaccines)
 def mvaccVaccine = ''
-println(opensrpVaccineArray)
-println(vaccineArray)
+def eventList = []
 vaccineArray.each {
     mvaccVaccine = it
     def vaccineSequence = defaultVaccineSequence(mvaccVaccine)
@@ -43,7 +57,79 @@ vaccineArray.each {
             }
         }
     }
-    println vaccineSequence.toString() + mvaccVaccine
+    def currentDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(new Date())
+    def vaccineDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+
+    Obs vaccineObsDate = generateObs(vaccineSequence, conceptMap.getAt(mvaccVaccine), "date", mvaccVaccine.toLowerCase(), vaccineDate)
+    Obs vaccineObsCalculate = generateObs(vaccineSequence, conceptMap.getAt(mvaccVaccine), "calculate", mvaccVaccine.toLowerCase(), vaccineSequence.toString())
+    Obs[] vaccineObs = [vaccineObsDate, vaccineObsCalculate]
+
+    Event vaccineEvent = new Event(
+            type: 'Event',
+            dateCreated: currentDate,
+            baseEntityId: clientUUID,
+            locationId: locationId,
+            eventDate: currentDate,
+            eventType: 'Vaccination',
+            formSubmissionId: UUID.randomUUID().toString(),
+            providerId: providerId,
+            entityType: 'vaccination',
+            duration: 0,
+            obs: vaccineObs
+    )
+    eventList.add(vaccineEvent)
+
+}
+
+println(groovy.json.JsonOutput.toJson(events: eventList))
+
+class Event {
+    String type
+    String dateCreated
+    String baseEntityId
+    String locationId
+    String eventDate
+    String eventType
+    String formSubmissionId
+    String providerId
+    String entityType
+    Obs[] obs
+    int duration
+}
+
+class Obs {
+    String fieldType
+    String fieldDataType
+    String fieldCode
+    String parentCode
+    String[] values
+    String formSubmissionField
+    String[] humanReadableValues
+}
+
+Obs generateObs(int calculate, String vaccineConcept, String fieldDataType, String formSubmissionField, String[] values) {
+    Obs obs = new Obs()
+    String[] humanReadableValues = new String()
+    obs.setProperty("fieldType", 'concept')
+    obs.setProperty("fieldDataType", fieldDataType)
+    obs.setProperty("parentCode", vaccineConcept)
+    if (formSubmissionField.equals("bcg") && calculate > 1)
+        formSubmissionField = formSubmissionField + calculate
+    else
+        formSubmissionField = formSubmissionField + "_" + calculate
+
+    if (fieldDataType.equals("date")) {
+        obs.setProperty("fieldCode", '1410AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+        obs.setProperty("formSubmissionField", formSubmissionField)
+    } else {
+        obs.setProperty("fieldCode", '1418AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+        obs.setProperty("formSubmissionField", formSubmissionField + '_dose')
+    }
+
+    obs.setProperty("values", values)
+    obs.setProperty("humanReadableValues", humanReadableValues)
+
+    return obs
 }
 
 int defaultVaccineSequence(String vaccineName) {
@@ -52,8 +138,6 @@ int defaultVaccineSequence(String vaccineName) {
     }
     return 1
 }
-
-
 
 
 
